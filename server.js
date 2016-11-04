620994962f4a60aa25a36938bd159c6c04145031
Fortune <fortuneochi@gmail.com>
2016-11-04 07:43:03 +0000

    request header project server completed

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var app = require("express")();
-app.get('/',(req,res)=>{
-   res.send("hello");
+// var requestIp = require('request-ip');
+var os = require("os");
+
+app.get('/api/whoami', (req, res) => {
+   var json = {
+      ipaddress:req.headers['x-forwarded-for'],
+      language:req.headers['accept-language'].split(',')[0],
+      software:req.headers['user-agent'].split('(')[1].split(')')[0]
+   }
+   res.end(JSON.stringify(json,null,1));
 });
 app.listen(8080);
